ca1df88371afb7ccc534e87c85372589ec6d3475
zacharygilliom <zacharygilliom@gmail.com>
2020-02-09 08:19:48 -0500

    Adding images, updated readme, cleaned up code with more specific functions, and added comments.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # wine-reviews
+
+Wine tasting can be a very subjective experience.  Someone who is very new to wine tasting is probably no going to have the same appreciation for subtle tastes as someone who has done a lot more wine tasting.  So I tried to focus on wine tasters who had a lot of reviews as there opinions would most likely be more accurate of the general opinion.
+
+![wine_taster_x_points](https://user-images.githubusercontent.com/23482152/74114614-185b8b00-4b79-11ea-8f24-af6e8261009e.png)
+
+I also wanted to take a look at the countries that had the most reviews as they would have a better gauge on which countries had the best wine.
+
+![Countries_x_Points](https://user-images.githubusercontent.com/23482152/74114591-fbbf5300-4b78-11ea-9224-76fd34d79daa.png)
+
+It's important to take a look at some of the unique values too.  Are a lot of our reviews spread out over a wide area?  Are they very specific to a specific type of wine?  How many different wine tasters participated?
+
+![unique_values](https://user-images.githubusercontent.com/23482152/74114641-4c36b080-4b79-11ea-8037-3fc8660c2bd1.png)
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import seaborn as sns
 import matplotlib.pyplot as plt 
 import numpy as np 
 
+sns.set_style('darkgrid')
+sns.set_palette("GnBu_d")
+
 
 # Some basic summary stats to get an idea of how our data is shaped
 def summary(df):
@@ -18,29 +21,9 @@ def summary(df):
 	print('***** The Number of Unique Items in Each Column *****')
 	for col in df.columns:
 		print(str(col) + ':\n' + str(np.count_nonzero(df[col].unique())) + '\n')
+	get_unique(df)
 
-
-def plot(df):
-	# Plotting some of our location data and a distribution of our price data.
-	plt.subplot(311)
-	sns.barplot(x='price', y='region_2', data=df)
-
-	plt.subplot(312)
-	sns.barplot(x='price', y='taster_name', data=df)
-
-	plt.subplot(313)
-	sns.kdeplot(data=df['price'], shade=True)
-
-	plt.show()
-
-
-def df_country(df, country):
-	df = df[df['country'] == country]
-	summary(df)
-	plot(df)
-	return df
-
-
+# Useful to find the number of unique values for some of our categorical variables to get an idea of the shape of our dataset.
 def get_unique(df):
 	print('\n**** Unique values for country ****')
 	print(df['country'].unique())
@@ -54,31 +37,70 @@ def get_unique(df):
 	print(df['taster_name'].unique())
 
 
-def hist_plots(df):
-	a = df['taster_name'].unique()
-	print(a)
-	b = range(1, len(df['taster_name'].unique()))
-	print(list(b))
-	c = len(df['taster_name'].unique())
-	print(c)
+# Given the NA values in our countries column, it would be easier to just remove the rows that don't have a country value.
+def clean_country_na(df):
+	df.dropna(subset=['country'], inplace=True)
+	return df
+
+# We will use the next two functions to create the histogram plot of all of our wine tasters and countres.  
+# We wil plot all of them on the same axis and we will only plot the top values.
+def hist_plots_taster_name(df):
+	a = get_v_counts(df, 'taster_name')
+	b = range(1, len(a) + 1)
+	c = len(a)
 	for i, j in zip(a, b):
-		print(i, j)
-		df1 = df[df['taster_name'] == i]
-		plt.subplot(c, 1, j)
-		sns.kdeplot(data=df1['price'], shade=True)
+		sns.kdeplot(data=df[df['taster_name'] == i]['points'], shade=False, label=i, kernel='gau')
+	plt.title('KDE Plots of Points for the top Wine Tasters by number of reviews')
+	plt.xlabel('Points')
+	plt.ylabel('KDE Distribution')
+
+
+def hist_plots_country(df):
+	a = get_v_counts(df, 'country')
+	b = range(1, len(a) + 1)
+	c = len(a)
+	for i, j in zip(a, b):
+		sns.kdeplot(data=df[df['country'] == i]['points'], shade=False, label=i, kernel='gau')
+	plt.title('KDE Plots of Points for the top Countries by number of reviews')
+	plt.xlabel('Points')
+	plt.ylabel('KDE Distribution')
+
+# want to find the top column values in all the rows, regardless of how many distinct values there are.
+def get_top_v(df, col):
+	sum_list = []
+	for w in df[col].unique():
+		df1 = df[df[col] == w]
+		sum_list.append(df1.shape[0])
+	total = sum(sum_list)
+	avg = total / (1.25 * np.count_nonzero(df[col].unique()))
+	return avg
+
+# Add the top values to a list
+def get_v_counts(df, col):
+	top_v = []
+	for v in df[col].unique():
+		df1 = df[df[col] == v]
+		if df1.shape[0] > get_top_v(df, col):
+			top_v.append(v)
+	return top_v
+
+# Create our figures, subplots, and some formatting
+def plot(df):
+	df = clean_country_na(df)
+	figure, axes = plt.subplots(nrows=2, ncols=1)
+	plt.subplot(211)
+	hist_plots_country(df)
+	plt.subplot(212)
+	hist_plots_taster_name(df)
+	figure.tight_layout(pad=0.5)
 	plt.show()
 
+# Load in our dataset
 df = pd.read_csv('winemag-data-130k-v2.csv')
-# summary(df)
 
-# based on the shape of our data, the majority of our price data is under $100.
-# df = df[df['price'] < 200]
+# First we want to print some summary stats of our dataset
+summary(df)
 
-# summary(df)
+# PLotting our data
+plot(df)
 
-# print(df_country(df, 'US'))
-# plot(df)
-
-# get_unique(df)
-
-hist_plots(df)
